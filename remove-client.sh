21ba5526f962adbb29f733bbf1f79dbd5de8b15f
Rick Dawson <dawsora@gmail.com>
2021-10-07 15:10:15 -0400

    Clean up script formatting

--- remove-client.sh
+++ remove-client.sh
@@ -5,9 +5,16 @@ then
 	echo "You must specify a valid client name or public key"
 	sudo wg show
 	exit 1
-else if [ $(echo "${$1: -1}") == "=" ]
+elif [ $(echo "${$1: -1}") == "=" ] 
 then
-	peer_pub_key=$1
+	wg_server=$(sudo wg show)
+	if [[ "$wg_server" == *"$1"* ]]
+	then
+		peer_pub_key=$1
+	else
+		echo "Public key" $1 "not valid"
+		exit 1
+	fi
 else
 	echo "Removing" $1
 	# Check to see if client exists

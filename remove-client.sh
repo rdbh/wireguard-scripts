8ebbc000183f12a91dac71208f17581a64df3b37
Rick Dawson <dawsora@gmail.com>
2021-10-07 14:31:16 -0400

    renamed remove-peer to remove-client for consistency. Added lookup for peer name to remove-client

---
+++ remove-client.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+	echo "You must specify a valid client name or public key"
+	sudo wg show
+	exit 1
+else if [ $(echo "${$1: -1}") == "=" ]
+then
+	peer_pub_key=$1
+else
+	echo "Removing" $1
+	# Check to see if client exists
+	if [ -f /clients/$1/$1.pub ]
+	then
+		peer_pub_key=$(cat /clients/$1/$1.pub)
+	else
+		echo "Can't find config for client " $1
+		exit 1
+	fi
+fi
+echo "Removing" $1
+sudo wg set wg0 peer $peer_pub_key remove
+sudo wg show

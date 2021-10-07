f9f9d165a70df4cc0ef25366e1e00851f22f426c
Rick Dawson <dawsora@gmail.com>
2021-10-07 12:16:12 -0400

    Clarified the language of remove-client

--- remove-peer.sh
+++ remove-peer.sh
@@ -2,8 +2,10 @@
 
 if [ $# -eq 0 ]
 then
-	echo "must have peer id as arg: remove-peer.sh <asdf123=>"
+	echo "must have peer public key as arg: remove-peer.sh <asdf123=>"
+	wg show
 else
+	echo "Removing" $1
 	sudo wg set wg0 peer $1 remove
 	sudo wg show
 fi
